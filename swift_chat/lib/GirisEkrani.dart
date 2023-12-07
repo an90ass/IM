f116f349401712a9ed1e3962332667d9cfc9e39d
an90ass <21670310176@ogrenci.bartin.edu.tr>
2023-12-08 01:05:54 +0300

    Giris sayfasi olusturdum

---
+++ swift_chat/lib/GirisEkrani.dart
@@ -0,0 +1,149 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, file_names, sized_box_for_whitespace, use_key_in_widget_constructors, library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+
+class GirisEkrani extends StatefulWidget {
+  @override
+  _GirisEkraniState createState() => _GirisEkraniState();
+}
+
+class _GirisEkraniState extends State<GirisEkrani> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: isLoading
+          ? Center(
+              child: Container(
+                height: size.height / 20,
+                width: size.height / 20,
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: size.height / 20, // Ekranın üst kısmında boşluk bırak
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    width: size.width / 0.5,
+                    child: IconButton(
+                        icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
+                  ),
+                  SizedBox(
+                    height: size.height / 50,
+                  ),
+                  Container(
+                    width: size.width / 1.1, // soldan
+                    child: Text(
+                      "Merhaba",
+                      style: TextStyle(
+                        fontSize: 34,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: size.width / 1.1,
+                    child: Text(
+                      "Devam etmek için oturum açın!",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: size.height / 10,
+                  ),
+                  Container(
+                    width: size.width,
+                    alignment: Alignment.center,
+                    child: field(size, "email", Icons.account_box, _email),
+                  ),
+                  Padding( // email text ve password text arasinda bosluk birak
+                    padding: const EdgeInsets.symmetric(vertical: 18.0),
+                    child: Container(
+                      width: size.width,
+                      alignment: Alignment.center,
+                      child: field(size, "şifre", Icons.lock, _password),
+                    ),
+                  ),
+                  SizedBox(
+                    height: size.height / 10,
+                  ),
+                  customButton(size),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () => Navigator.pop(context),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Hesabınız yok mu? "),
+                          GestureDetector(
+                            onTap: () {
+                              // Navigator.pushNamed(context, "/");//HesapOlusturmaEkrani daha sonra olusturacagim
+                            },
+                            child: Text(
+                              " Hesap oluştur",
+                              style: TextStyle(fontWeight: FontWeight.w700, color: Colors.blue),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+
+// eamil ve sifre icin
+  Widget field(Size size, String label, IconData icon, TextEditingController controller) {
+    return Container(
+      height: size.height / 15,
+      width: size.width / 1.1,
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          prefixIcon: Icon(icon),
+          contentPadding: EdgeInsets.all(15),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget customButton(Size size) {
+    return Container(
+      height: size.height / 14,
+      width: size.width / 1.2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.blue,
+      ),
+      alignment: Alignment.center,
+      child: Text(
+        "Giriş yap",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
