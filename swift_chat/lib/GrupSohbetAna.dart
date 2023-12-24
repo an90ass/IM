820dfee39e455a37755eca61c51973f0562a0adc
Hamza Almahmoud <101905387+Hamza7m@users.noreply.github.com>
2023-12-25 02:34:55 +0300

    updated

--- swift_chat/lib/GrupSohbetAna.dart
+++ swift_chat/lib/GrupSohbetAna.dart
@@ -1,5 +1,7 @@
-// ignore_for_file: unused_import, prefer_const_constructors, file_names, unused_local_variable, sort_child_properties_last
+// ignore_for_file: unused_import, prefer_const_constructors, file_names, unused_local_variable, sort_child_properties_last, avoid_print
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:swift_chat/GrubSohbetOdasi2.dart';
 import 'package:swift_chat/add_members.dart';
@@ -12,6 +14,42 @@ class GrubSohbetAna extends StatefulWidget {
 }
 
 class _GrubSohbetAnaState extends State<GrubSohbetAna> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+
+
+  bool isLoading = true;
+  List groupList = [];
+  @override
+  void initState() {
+    super.initState();
+    getAvailableGroups();
+  }
+
+  void getAvailableGroups() async {
+    // String? uid = _auth.currentUser?.uid;
+    // await _auth.currentUser?.reload();
+    // String? uid = _auth.currentUser?.uid;
+      String uid = _auth.currentUser!.uid;
+
+ print("******************************غ4444444444444");
+    print(uid);
+
+    await _firestore
+        .collection('users')
+        .doc(uid)
+        .collection('groups')
+        .get()
+        .then((value) {
+      setState(() {
+        groupList = value.docs;
+        // isLoading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -20,23 +58,30 @@ class _GrubSohbetAnaState extends State<GrubSohbetAna> {
       appBar: AppBar(
         title: Text("Gruplar"),
       ),
-      body: ListView.builder(
-        itemCount: 5,
+      body:
+          //  : isLoading? Container(
+          //   height: size.height,
+          //   width: size.width,
+          //   alignment: Alignment.center,
+          //   child: CircularProgressIndicator(),
+//):
+          ListView.builder(
+        itemCount: groupList.length,
         itemBuilder: (context, index) {
           return ListTile(
             onTap: () => Navigator.of(context).push(MaterialPageRoute(
               builder: (_) => GrubSohbetOdasi2(),
             )),
             leading: Icon(Icons.group),
-            title: Text("Grub $index"),
+            title: Text(groupList[index]['name']),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.create),
-        onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: 
-        (_) =>AddMembersInGroup()
-        ),),
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(builder: (_) => AddMembersInGroup()),
+        ),
         tooltip: "Grup olustur",
       ),
     );
--- swift_chat/lib/add_members.dart
+++ swift_chat/lib/add_members.dart
@@ -1,14 +1,105 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unused_field
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:swift_chat/gruplar_olusturma.dart';
 
-class AddMembersInGroup extends StatelessWidget {
+class AddMembersInGroup extends StatefulWidget {
   const AddMembersInGroup({super.key});
 
   @override
+  State<AddMembersInGroup> createState() => _AddMembersInGroupState();
+}
+
+class _AddMembersInGroupState extends State<AddMembersInGroup> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final TextEditingController _search = TextEditingController();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Map<String, dynamic>? userMap;
+  bool isLoading = false;
+  // List membersList = [];
+  List<Map<String, dynamic>> membersList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUserDeta();
+  }
+
+  void getCurrentUserDeta() async {
+    await _firestore
+        .collection('users')
+        .doc(_auth.currentUser!.uid)
+        .get()
+        .then((userMap) {
+      setState(() {
+        membersList.add({
+          "name": userMap['name'],
+          "email": userMap['email'],
+          "uid": userMap['uid'],
+          "isAdmin": true,
+        });
+      });
+    });
+  }
+
+  void onRemoveMembers(int index) {
+    if (membersList[index]['uid'] != _auth.currentUser?.uid) {
+      setState(() {
+        membersList.removeAt(index);
+      });
+    }
+  }
+
+  void onSearch() async {
+    setState(() {
+      isLoading = true;
+    });
+    print("Text in the search field: ${_search.text}");
+    await _firestore
+        .collection("users")
+        .where("email", isEqualTo: _search.text.trim())
+        .get()
+        .then((value) {
+      setState(() {
+        if (value.docs.isNotEmpty) {
+          userMap = value.docs[0].data();
+        } else {
+          userMap = null;
+        }
+        isLoading = false;
+      });
+
+      print(userMap);
+    });
+  }
+
+  bool isAlreadyExist = false;
+
+  void onResulTap() {
+    for (int i = 0; i < membersList.length; i++) {
+      if (membersList[i]['uid'] == userMap?['uid']) {
+        isAlreadyExist = true;
+      }
+    }
+    if (!isAlreadyExist) {
+      setState(() {
+        membersList.add({
+          "name": userMap!['name'],
+          "email": userMap!['email'],
+          "uid": userMap!['uid'],
+          "isAdmin": false,
+        });
+        userMap = null;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-        final Size size = MediaQuery.of(context).size;
+    final Size size = MediaQuery.of(context).size;
 
     return Scaffold(
       appBar: AppBar(
@@ -18,62 +109,77 @@ class AddMembersInGroup extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Flexible(child: ListView.builder(
-              itemCount: 3,
-              shrinkWrap: true,
-              physics:NeverScrollableScrollPhysics(),
-              
-              itemBuilder:(context,index){
-                return ListTile(
-                  onTap: (){},
-                  leading: Icon(Icons.account_circle),
-                  title: Text("User2"),
-                  trailing: Icon(Icons.close),
-                );
-              } 
-             
-            ),),
-            SizedBox(
-            height:size.height/20 ,
+            Flexible(
+              child: ListView.builder(
+                  itemCount: membersList.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      onTap: () => onRemoveMembers(index),
+                      leading: Icon(Icons.account_circle),
+                      title: Text(membersList[index]['name']),
+                      subtitle: Text(membersList[index]['email']),
+                      trailing: Icon(Icons.close),
+                    );
+                  }),
             ),
-              Container(
-                  height: size.height / 14,
-                  width: size.width,
-                  child: Container(
-                    height: size.height / 14,
-                    width: size.width / 1.2,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: "Ara",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
+            SizedBox(
+              height: size.height / 20,
+            ),
+            Container(
+              height: size.height / 14,
+              width: size.width,
+              child: Container(
+                height: size.height / 14,
+                width: size.width / 1.2,
+                child: TextField(
+                  controller: _search,
+                  decoration: InputDecoration(
+                    hintText: "Ara",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: size.height / 30,
-                ),
-                ElevatedButton(
-                  onPressed: (){},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
+              ),
+            ),
+            SizedBox(
+              height: size.height / 30,
+            ),
+            isLoading
+                ? Container(
+                    height: size.height / 10,
+                    width: size.width / 12,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(),
+                  )
+                : ElevatedButton(
+                    onPressed: onSearch,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: Text("Ara"),
                   ),
-                  child: Text("Ara"),
-                ),
+            userMap != null
+                ? ListTile(
+                    onTap: onResulTap,
+                    leading: Icon(Icons.account_box),
+                    title: Text(userMap!['name']),
+                    subtitle: Text(userMap!['email']),
+                    trailing: Icon(Icons.add),
+                  )
+                : SizedBox(),
           ],
         ),
       ),
-
-
-
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.forward),
-        onPressed: () =>Navigator.of(context).push(
-          MaterialPageRoute(builder: (_) =>GrouplarOlusturma())
-        ),
-      ),
+      floatingActionButton: membersList.length >= 2
+          ? FloatingActionButton(
+              child: Icon(Icons.forward),
+              onPressed: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (_) => GrouplarOlusturma(membersList:membersList,))),
+            )
+          : SizedBox(),
     );
   }
 }
